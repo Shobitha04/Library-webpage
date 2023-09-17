3a52c6e9e97d47dace6734c2d6669722b1020a20
M Shobitha <96161378+Shobitha04@users.noreply.github.com>
2023-09-17 16:31:39 +0530

    Create emphasize.java

---
+++ emphasize.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    String txt = "We are the so-called \"Vikings\" from the north.";
+    System.out.println(txt);
+    String txtt = "he is the \'real\' hero of the story";
+    System.out.println(txtt);
+  }
+}
